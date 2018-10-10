2c98972e18bd905584b02c1635707dd0e257f37e
Tobi Balogun <tobybarlhogun@gmail.com>
2018-10-10 13:31:17 -0700

    Added mouse Interaction

--- public/Shapes/Circle.js
+++ public/Shapes/Circle.js
@@ -1,27 +1,33 @@
 export default class Circle {
-    constructor(context, x, y, color, radius) {
+    constructor(context, x, y, color, radius, stroke) {
         this.ctx = context;
         this.x = x;
         this.y = y;
         this.color = color;
         this.radius = radius;
+        this.stroke = stroke;
         this.speed = this.randomSpeed(0.01, 0.05);
+
         console.log(this.speed)
     }
     draw() {
-        this.ctx.fillStyle = this.color;
+        let isStroke = this.stroke ? true : false;
+        if (isStroke) {
+            this.ctx.fillStyle = this.color;
+        } else {
+            this.ctx.strokeStyle = this.color;
+        }
         this.ctx.beginPath();
         this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
         this.ctx.closePath();
-        this.ctx.fill();
+        if (isStroke) {
+            this.ctx.fill();
+        } else {
+            this.ctx.stroke()
+        }
+
 
         return this.ctx;
-    }
-    animate() {
-        // this.x += Math.cos(angToRad(45)) * this.speed;
-        // this.y += Math.cos(angToRad(45)) * this.speed;
-        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-        this.ctx.rotate(Math.cos(180 * Math.PI / 180))
     }
     randomSpeed(min, max) {
         return Math.random() * (max - min + 1) + min;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import * as dat from 'dat.gui';
-import { randomInt, dist, angToRad, colors } from '../public/utils';
+import { TweenMax, Power3 } from 'gsap'
+import { randomInt, dist, angToRad, colors } from '../public/utils/index';
 import Circle from '../public/Shapes/Circle';
 import Rect from '../public/Shapes/Rect'
-import Point from '../public/Point'
+import Point from '../public/Shapes/Point'
 
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
@@ -38,14 +39,14 @@ let rectangles = [];
 
 const circleFolder = gui.addFolder('circles');
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 500; i++) {
     // ctx.arc(i * 10, i + 10, 50, 0, Math.PI / 180, false);
 
     const p1 = new Point(randomInt(0, WIDTH), randomInt(0, HEIGHT));
     // const p2 = new Point((randomInt(0, HEIGHT), randomInt(0, HEIGHT)));
 
     // ctx.translate(window.innerWidth / 2, window.innerHeight / 2);
-    const circle = new Circle(ctx, p1.x, p1.y, randomColor(colors), randomInt(1, 10));
+    const circle = new Circle(ctx, p1.x, p1.y, randomColor(colors), randomInt(1, 10), true);
     const rect = new Rect(ctx, p1.x, p1.y, randomInt(2, 10), randomInt(2, 10), randomColor(colors));
 
 
@@ -68,19 +69,33 @@ for (let i = 0; i < 100; i++) {
 
 function animate() {
     requestAnimationFrame(animate);
-    background('blue')
+
     for (let i = 0; i < circles.length; i++) {
         const circle = circles[i];
-        // circle.speed += 0.005;
-        // circle.x += Math.cos(angToRad(15)) * circle.speed;
-        // circle.y += Math.cos(angToRad(15)) * circle.speed;
-        circle.animate()
-
         circle.draw();
 
 
     }
 
 }
+
+// Mouse interaction
+canvas.addEventListener('mousemove', (e) => {
+    const x = e.pageX;
+    const y = e.pageY;
+
+    circles.forEach((circle, index) => {
+        TweenMax.to(circle, index * 0.01, {
+                x: x,
+                y: y,
+                ease: Expo.easeInOut
+            })
+            // setTimeout(() => {
+            //     circle.x = x;
+            //     circle.y = y;
+            // }, index * 10)
+
+    })
+})
 
 animate();
